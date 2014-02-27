862f905d2ce36c7247f02a72f86394d9483b20b5
dahopkin <dahopkin2@gmail.com>
2014-02-27 01:48:07 -0500

    Updated the README file.

--- Invaders/Properties/AssemblyInfo.cs
+++ Invaders/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Invaders")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Invaders")]
 [assembly: AssemblyProduct("Invaders")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
